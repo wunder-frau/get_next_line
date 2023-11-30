4b0643d47265e464795f51a50953f6a1772a49e3
Iryna Stasheuski <istasheu@student.hive.fi>
2023-11-30 10:47:32 +0200

    develop: get_next_line_utils.c uptate with ft_strchr

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 08:30:59 by istasheu          #+#    #+#             */
-/*   Updated: 2023/11/30 09:09:52 by istasheu         ###   ########.fr       */
+/*   Updated: 2023/11/30 10:46:42 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,17 @@ size_t	ft_strlen(const char *str)
 	while(*p)
 		p++;
 	return ((size_t)(p - str));
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while(*s || (unsigned char)c == "\0")
+	{
+		if (*s == (unsigned char)c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	return (0);
 }
